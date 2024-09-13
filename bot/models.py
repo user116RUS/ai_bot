8ecf11e176a1d957dbad56221bc12c378dcc890d
printnewcode <zenit200803@mail.ru>
2024-09-13 17:40:18 +0300

    Models.py improving

--- bot/models.py
+++ bot/models.py
@@ -2,27 +2,78 @@ from django.db import models
 
 
 class User(models.Model):
-    telegram_id = models.IntegerField(max_length=10, primary_key=True)
+    telegram_id = models.IntegerField(max_length=15, primary_key=True)
     name = models.CharField(max_length=35, verbose_name="Имя")
-    # history =
-    # UserRequest =
+
+    # message_context = История переписки пользователя
+    def __str__(self):
+        return self.telegram_id
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователь'
 
 
-class Request(models.Model):
-    name = models.CharField(max_length=35, verbose_name="Имя")
-    model = models.CharField(max_length=20, verbose_name="Модель ИИ")
+class Mode(models.Model):
+    name = models.CharField(max_length=35, verbose_name="Название")
+    model = models.CharField(max_length=50, verbose_name="Модель ИИ")
     max_token = models.IntegerField(max_length=8)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Мод ИИ'
+        verbose_name_plural = 'Моды ИИ'
 
 
 class Referal(models.Model):
-    inviter = User.telegram_id
+    inviter = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Пригласитель',
+        related_name='referal'
+    )
     is_used = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.inviter.name
 
-class UserRequest:
-    ...
+    class Meta:
+        verbose_name = 'Реферал'
+        verbose_name_plural = 'Рефералки'
+
+
+class UserMode:
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Пользователь',
+        related_name='user_mode'
+    )
+    mode = models.ForeignKey(
+        Mode,
+        on_delete=models.CASCADE,
+        verbose_name="Мод",
+        related_name="user_mode"
+    )
+    requests_amount = models.IntegerField(verbose_name='Количество запросов')
+
+    def __str__(self):
+        return self.user.name, self.mode.model, self.requests_amount
+
+    class Meta:
+        verbose_name = 'Юзер-Мод'
+        verbose_name_plural = 'Юзер-Моды'
 
 
 class Prompt(models.Model):
-    text = models.CharField(max_length=500, verbose_name="Текст промпта")
-    name = models.CharField(max_length=30, verbose_name="Название промпта")
+    text = models.CharField(verbose_name="Текст промпта")
+    name = models.CharField(max_length=50, verbose_name="Название промпта")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Промпт'
+        verbose_name_plural = 'Промпты'
