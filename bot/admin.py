9e11ed4ceaa4287e5f692ccbe14b2c46b19c6253
printnewcode <zenit200803@mail.ru>
2024-09-19 18:57:12 +0300

    Admin panel(admin.py)

--- bot/admin.py
+++ bot/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from .models import (
+    User,
+    Mode,
+    Referal,
+    UserMode,
+    Prompt,
 
-# Register your models here.
+)
+
+
+class ModeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'model', 'max_token', ]
+    list_display_links = ['name']
+    search_fields = ['name', 'model', ]
+    ordering = ['max_token', ]
+
+
+class PromptAdmin(admin.ModelAdmin):
+    list_display = ['text', 'name', ]
+    list_display_links = ['name', ]
+    search_fields = ['name', ]
+
+
+class ReferalAdmin(admin.ModelAdmin):
+    list_display = ['inviter', 'is_used', ]
+    search_fields = ['inviter', ]
+    ordering = ['is_used', ]
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['telegram_id', 'name', ]
+    list_display_links = ['telegram_id', ]
+    search_fields = ['telegram_id', 'name', ]
+
+
+class UserModeAdmin(admin.ModelAdmin):
+    list_display = ['user', 'mode', 'request_amount']
+    list_display_links = ['user', ]
+    search_fields = ['name', 'mode', ]
+    ordering = ['request_amount', ]
+
+
+admin.site.register(Mode, ModeAdmin)
+admin.site.register(Prompt, PromptAdmin)
+admin.site.register(Referal, ReferalAdmin)
+admin.site.register(User, UserAdmin)
+admin.site.register(UserMode, UserModeAdmin)
