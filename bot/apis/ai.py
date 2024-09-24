703ca6a51cb5214337f638793cdae4b2abbac2e8
Alim <scorty716@gmail.com>
2024-09-24 19:58:36 +0300

    some fixes

--- bot/apis/ai.py
+++ bot/apis/ai.py
@@ -1,17 +1,17 @@
-import os
+from django.conf import settings
 
-import telebot
+import base64
+import os
 
 import dotenv
 import openai
-import base64
-import requests
+import telebot
 
 
 dotenv.load_dotenv()
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
-
+openai.api_key = os.getenv("OPENAI_API_KEY") if settings.PROVIDER_NAME == "openai" else os.getenv("VSEGPT_API_KEY")
+openai.base_url = settings.PROVIDER
 
 ASSISTANT_PROMPT = ()
 ANALYTIC_PROMPT = ()
@@ -19,13 +19,12 @@ ANALYTIC_PROMPT = ()
 ERROR="Извините, что-то пошло не так"
 
 def encode_image(image):
-  with open(image, "rb") as image_file:
-    return base64.b64encode(image_file.read()).decode('utf-8')
+    with open(image, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
+
 
 class BaseAIAPI:
     def __init__(self, ) -> None:
-        self._CHAT_MODEL: str = "openai/gpt-4o-mini"  # "openai/gpt-4o-mini"
-        self._ANALYTIC_MODEL: str = "openai/gpt-4o-mini"
         self._MAX_TOKENS: int = 1000
         self._ASSISTANT_PROMPT: str = ASSISTANT_PROMPT
         self._ANALYTIC_PROMPT: str = ANALYTIC_PROMPT
@@ -39,87 +38,50 @@ class OpenAIAPI(BaseAIAPI):
     def __init__(self,) -> None:
         super().__init__()
     
-    def _get_or_create_user_chat_history(self, chat_id: int, new_user_message: str = "") -> list:
-        """
-        Retrieves or creates the chat history for a given user.
-
-        Args:
-            chat_id (int): The unique identifier for the user's chat.
-            new_user_message (str): The new message from the user to be appended to the chat history. Defaults to an empty string.
-
-        Returns:
-            list: The updated chat history for the given user.
-        """
+    def _get_or_create_user_chat_history(self, chat_id: int, new_user_image: str = "", new_user_message: str = "") -> list:
         if not self.chat_history.get(chat_id, False):
             self.chat_history[chat_id] = []
             self.chat_history[chat_id].append(
                 {"role": "system", "content": self._ASSISTANT_PROMPT})
-            self.chat_history[chat_id].append(
-                {"role": "user", "content": new_user_message})
-            return self.chat_history[chat_id]
+            if new_user_image != "":
+                self.chat_history[chat_id].append(
+                    {"role": "user", "content": [
+                        {"type": "text", "text": new_user_message},
+                        {"type": "image_url", "image_url": new_user_image}
+                    ]})
+                return self.chat_history[chat_id]
+            elif new_user_image == "":
+                self.chat_history[chat_id].append(
+                    {"role": "user", "content": {"type": "text", "text": new_user_message}})
+                return self.chat_history[chat_id]
 
         self.chat_history[chat_id].append(
-            {"role": "user", "content": new_user_message})
-        chat_history = self.chat_history[chat_id]
-        return chat_history
-    def _get_or_create_user_chat_history_to_work_with_images(self, chat_id: int, new_user_message: str = "", new_user_image: str = "") -> list:
-        if not self.chat_history.get(chat_id, False):
-            self.chat_history[chat_id] = []
-            self.chat_history[chat_id].append(
-                {"role": "system", "content": self._ASSISTANT_PROMPT})
-            self.chat_history[chat_id].append(
                 {"role": "user", "content": [
-                    {"type": "text", "text": new_user_message},
-                    {"type": "image_url", "image_url": new_user_image}
-                ]})
-            return self.chat_history[chat_id]
-
-        self.chat_history[chat_id].append(
-            {"role": "user", content": [
                     {"type": "text", "text": new_user_message},
                     {"type": "image_url", "image_url": new_user_image}
                 ]})
         chat_history = self.chat_history[chat_id]
         return chat_history
     
-    def get_response_only_text(self, chat_id: int, text: str) -> str:
-        user_chat_history = self._get_or_create_user_chat_history(
-            chat_id, text)
-        
+    def get_response(self, chat_id: int, text: str, model: str, image: str = "") -> str:
         try:
+            if image != "":
+                base64_image = encode_image(image)
+                user_chat_history = self._get_or_create_user_chat_history_to_work_with_images(chat_id, text, base64_image)
+            else:
+                user_chat_history = self._get_or_create_user_chat_history_to_work_with_images(chat_id, text)
             response = ( 
                 openai.chat.completions.create(
-                model=self._CHAT_MODEL,
+                model=model,
                 messages=user_chat_history,
                 temperature=self._TEMPERATURE,
                 max_tokens=self._MAX_TOKENS,
-                stream=True
+                stream=False
             ).choices[0].message.content
             )
 
             self.chat_history[chat_id].append({"role": "assistant", "content": response})
             return response
-        except Exception as e:
-            return ERROR
-    
-    def get_response_image_plus_text(self, chat_id: int, image, text: str) -> str:
-        try:
-            user_chat_history = self._get_or_create_user_chat_history_to_work_with_images(chat_id, text, image)
-            
-            base64_image = encode_image(image)
 
-            headers = {
-            "Content-Type": "application/json",
-            "Authorization": f"Bearer {openai.api_key}"
-            }
-
-            payload = {
-            "model": "gpt-4o-mini",
-            "messages": user_chat_history,
-            "max_tokens": self._MAX_TOKENS
-            }
-
-            response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload).json()
-            return response
         except Exception as e:
             return ERROR
