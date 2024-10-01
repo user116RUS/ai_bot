61918c1b8b6acc4adeba7971fbac2f829e088ec2
printnewcode <zenit200803@mail.ru>
2024-10-01 18:49:48 +0300

    123

--- bot/handlers/customer.py
+++ bot/handlers/customer.py
@@ -1,19 +1,25 @@
 from telebot.types import (
-    Message, CallbackQuery
+    Message, CallbackQuery,    InlineKeyboardButton,
+    InlineKeyboardMarkup
 )
 from bot.models import Mode, UserMode, User
 from bot import bot, logger
 from bot.texts import HELP_TEXT, GREETING_TEXT, MODEL_TEXT
-from bot.keyboards import BUY_MENU
 
 
 
-
-def buy_message(call: CallbackQuery) -> None:
-    bot.delete_message(call.message.chat.id, call.message.message_id)
-    user = User.objects.get(telegram_id=call.message.from_user.id)
+def buy_message(callback: CallbackQuery) -> None:
+    chat_id = callback.message.chat.id
+    _, pk = callback.data.split('_')
+    bot.delete_message(chat_id, callback.message.id)
+    bot.delete_message(callback.message.chat.id, callback.message.message_id)
+    user = User.objects.get(telegram_id=callback.message.from_user.id)
     plan_name = user.mode.name
     model = user.mode.model
     amount = user.user_mode.requests_amount
     txt = f'ваш план: {plan_name} \nваша модель {model} \nоставшиеся запросы {amount}'
-    bot.send_message(call.message.chat.id, txt, reply_markup=BUY_MENU)
+    BUY_MENU = InlineKeyboardMarkup()
+    back = InlineKeyboardButton(text='назад', callback_data='back_choose_model')
+    buy = InlineKeyboardButton(text='купить', callback_data=f'pay_{pk}')
+    BUY_MENU.add(back).add(buy)
+    bot.send_message(callback.message.chat.id, txt, reply_markup=BUY_MENU)
--- bot/keyboards.py
+++ bot/keyboards.py
@@ -6,14 +6,15 @@ from telebot.types import (
     BotCommand,
     ReplyKeyboardRemove
 )
-from bot.models import Mode
+
+from bot.models import User
+
+
+
 
 """InlineKeyboards"""
 
-HUB_MENU = InlineKeyboardMarkup()
-buy_more_btn = InlineKeyboardButton(text='купить', callback_data='buy_message')
-change_model_btn = InlineKeyboardButton(text='сменить модель', callback_data='change_model_btn')
-HUB_MENU.add(buy_more_btn).add(change_model_btn)
+
 
 CHOOSE_MODEL_MENU = InlineKeyboardMarkup()
 model_1 = InlineKeyboardButton(text='1 модель сложный', callback_data='model_1')
@@ -21,10 +22,7 @@ model_2 = InlineKeyboardButton(text='2 модель легкий', callback_data
 model_3 = InlineKeyboardButton(text='3 модель легкий', callback_data='model_3')
 CHOOSE_MODEL_MENU.add(model_1).add(model_2).add(model_3)
 
-BUY_MENU = InlineKeyboardMarkup()
-back = InlineKeyboardButton(text='назад', callback_data='back_choose_model')
-buy = InlineKeyboardButton(text='купить', callback_data=f'buy_{Mode.name}')
-CHOOSE_MODEL_MENU.add(back).add(buy)
+
 
 """ReplyKeyboards"""
 
