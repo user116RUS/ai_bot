28b33048b6364f67210a92efac765a5a6aa54a3f
Alim <scorty716@gmail.com>
2024-10-31 19:04:18 +0300

    big changes but bugs still there

--- bot/apis/voice_recognition.py
+++ bot/apis/voice_recognition.py
@@ -1,15 +1,29 @@
-from pydub import AudioSegment
 from django.conf import settings
 
 import whisper
+import os
+import subprocess
+
+model_name = settings.WHISPER_MODEL
 
 class BaseVoiceRecognition:
-    def __init__(self):
-        self.model = whisper.load_model(settings.WHISPER_MODEL)
-
-class WhisperVoiceRecognition(BaseVoiceRecognition):
-    def __init__(self):
-        super().__init__()
-
-    def recognize(self, audio: AudioSegment) -> str:
+    def recognize(self, audio):
+        model = whisper.load_model(model_name)
+        try:
+            result = model.transcribe(audio)
+            return result["text"]
+        except Exception as e:
+            return e
+    
+    def convert_ogg_to_mp3(file_path):
+        from bot import logger
         
+        mp3_path = os.path.splitext(file_path)[0] + ".mp3"
+        subprocess.run(["ffmpeg", "-i", file_path, mp3_path])
+        if os.path.exists(mp3_path):
+            os.remove(file_path)
+            logger.info('audiofile converted to mp3')
+            return mp3_path
+        else:
+            logger.error(f"Conversion of {file_path} to mp3 failed.")
+            return None
--- bot/handlers/user/ai.py
+++ bot/handlers/user/ai.py
@@ -2,11 +2,12 @@ from telebot.types import (
     Message
 )
 
-from bot import AI_ASSISTANT, bot, logger
+from bot import AI_ASSISTANT, WHISPER_RECOGNITION, bot, logger
 from bot.core import check_registration
 from bot.models import Mode, UserMode, User
 from bot.texts import NOT_IN_DB_TEXT
 
+import os
 
 @check_registration
 def chat_with_ai(message: Message) -> None:
@@ -46,6 +47,54 @@ def chat_with_ai(message: Message) -> None:
         AI_ASSISTANT.clear_chat_history(user_id)
         logger.error(f'Error occurred: {e}')
 
+@bot.message_handler(content_types=["voice", "audio"])
+@check_registration
+def whisper_voice(message: Message) -> None:
+    """Whisper voice handler."""
+    user_id = message.chat.id
+
+    msg = bot.send_message(message.chat.id, 'Слушаю вопрос 🎶')
+
+    try:
+        user = User.objects.get(telegram_id=user_id)
+        user_modes = user.user_mode
+
+        for user_mode in user_modes.all():
+            if user_mode.is_actual is False:
+                pass
+            else:
+                try:
+                    file_info = bot.get_file(message.voice.file_id)
+                    converted_file_path = WHISPER_RECOGNITION.convert_ogg_to_mp3(file_info.file_path)
+                except:
+                    file_info = bot.get_file(message.audio.file_id)
+                    converted_file_path = WHISPER_RECOGNITION.convert_ogg_to_mp3(file_info.file_path)
+
+                text = WHISPER_RECOGNITION.recognize(converted_file_path)
+                bot.edit_message_text(chat_id=user_id, text='Думаю над ответом 💭', message_id=msg.message_id)
+                bot.send_chat_action(user_id, 'typing')
+
+                ai_mode = str(user_mode.mode.model)
+
+                if user_mode.requests_amount > 0:
+                    response = AI_ASSISTANT.get_response(chat_id=user_id, text=text, model=ai_mode)
+
+                    # Уменьшаем количество запросов на 1
+                    user_mode.requests_amount -= 1
+                    user_mode.save()  # Сохраняем изменения в базе данных
+                    bot.delete_message(user_id, msg.message_id)
+                    bot.send_message(user_id, response)
+
+                else:
+                    bot.delete_message(user_id, msg.message_id)
+                    bot.send_message(user_id, "У вас исчерпаны запросы. Пожалуйста, пополните баланс.")
+    except Exception as e:
+        bot.send_message(user_id, NOT_IN_DB_TEXT)
+        AI_ASSISTANT.clear_chat_history(user_id)
+        logger.error(f'Error occurred: {e}')
+
+    finally:
+        os.remove(converted_file_path)
 
 def clean_history(message: Message) -> None:
     """Clean AI chatting history."""
